0741a90fe8fbb4e990c01d35dda6aae451f26213
eslammuhammaad <eslammuhammaadd.67@gmail.com>
2023-12-27 10:16:10 +0200

    add store and redux to client

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { RootState } from "../redux/store";
 
 export default function SignIn() {
+  const [data, setData] = useState({});
+  const { loading, error } = useSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e: any) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const d = await res.json();
+      if (d.success === false) {
+        dispatch(signInFailure(d.message));
+        return;
+      }
+      dispatch(signInSuccess(d));
+      navigate("/");
+    } catch (error: any) {
+      dispatch(signInFailure(error.message));
+    }
+  };
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="font-bold my-7 text-3xl text-gray-700 text-center">
+        Sign In
+      </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-gray-700 text-white p-3 uppercase rounded-lg hover:opacity-85 disabled:opacity-60"
+        >
+          {" "}
+          {loading ? "Loading..." : "sign in"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,5 +12,20 @@ export const signup = async (req, res, next) => {
     res.status(201).json("New User Signed UP");
   } catch (err) {
     next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Not Found"));
+    const validPass = bcryptjs.compareSync(password, validUser.password);
+    if (!validPass) return next(errorHandler(401, "Wrong Password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const{password :pass,...rest}=validUser._doc
+    res.cookie("access_token", token, { httpOnly: true }).status(201).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
